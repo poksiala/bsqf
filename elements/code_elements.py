17d5a2c552df6f70e2c0cfc0d5128302038a2eac
panu <panu@oksiala.fi>
2016-12-03 03:41:07 +0200

    Added type constants

--- elements/code_elements.py
+++ elements/code_elements.py
@@ -21,6 +21,33 @@ class CodeSegment:
     def __str__(self):
         return self.write_out()
 
+class GenericElement:
+    # Types from OFP
+    STR = "String"
+    ARRAY = "Array"
+    BOOL = "Boolean"
+    GROUP = "Group"
+    NUM = "Number"
+    OBJ = "Object"
+    SIDE = "Side"
+    # Types from ARMA
+    CODE = "Code"
+    CONF = "Config"
+    CTRL = "Control"
+    DISP = "Display"
+    SCRPT = "Script(Handle)"
+    STRUCTURED = "Structured Text"
+    # Types from ARMA2
+    DIARY = "Diary_Record"
+    TASK = "Task"
+    TEAM_MEMBER = "Team_Member"
+    NAMESPACE = "Namespace"
+    TRANS = "Trans"
+    ORIENT = "Orient"
+    TARGET = "Target"
+    VECT = "Vector"
+
+
 class CodeElement:
 
     def __init__(self):
